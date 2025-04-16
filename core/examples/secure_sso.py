8e8ecf6bc5def29f1f2e85d80a5811c7b7add9b9
Carson <carsonburke22@gmail.com>
2025-04-16 16:38:38 -0700

    start of examples

--- core/examples/secure_sso.py
+++ core/examples/secure_sso.py
@@ -1,2 +1,24 @@
+from sso.fastcomments_sso import FastCommentsSSO
+from sso.secure_sso_user_data import SecureSSOUserData
+
+
 def main():
-    pass
+    api_key = "your_api_key_here"
+    # This should be done server side for security
+    user_data = SecureSSOUserData(
+        "user-123",
+        "email@example.com",
+        "John Doe",
+        "Avatar"
+    )
+    
+    # Create SSO config with payload
+    sso = FastCommentsSSO.new_secure(api_key, user_data)
+    
+    tenant_id = "tenant-123"
+    url_id = "123"
+    token = sso.create_token()
+    
+    # Create configuration
+    
+    # Interact with client
--- core/examples/simple_sso.py
+++ core/examples/simple_sso.py
@@ -1,2 +1,23 @@
+from sso.fastcomments_sso import FastCommentsSSO
+from sso.simple_sso_user_data import SimpleSSOUserData
+
+
 def main():
-    pass
+    api_key = "your_api_key_here"
+    # This should be done server side for security
+    user_data = SimpleSSOUserData(
+        "user-123",
+        "email@example.com",
+        "Avatar"
+    )
+    
+    # Create SSO config with payload
+    sso = FastCommentsSSO.new_simple(user_data)
+    
+    tenant_id = "tenant-123"
+    url_id = "123"
+    token = sso.create_token()
+    
+    # Create configuration
+    
+    # Interact with client
